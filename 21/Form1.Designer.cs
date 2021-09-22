83cb955895139a949edfc0d8cf32d0b331003237
greta <greta@DESKTOP-6N2E64G>
2021-09-22 19:16:59 +0200

    Add project files.

---
+++ 21/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿
+namespace _21
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::_21.Properties.Resources.szabály;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(47, 38);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(86, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(573, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "A játékosok felváltva húznak 1, 2 vagy 3 db gyufaszálat, aki felhúzza az utolsót " +
+    "az nyer";
+            this.label1.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.button2.Location = new System.Drawing.Point(139, 351);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 36);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "2 játékos";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.button3.Location = new System.Drawing.Point(528, 353);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(105, 35);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "gép ellen";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(22, 330);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Visible = false;
+            // 
+            // button4
+            // 
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.button4.Location = new System.Drawing.Point(36, 402);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(48, 36);
+            this.button4.TabIndex = 5;
+            this.button4.Tag = "1";
+            this.button4.Text = "1";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.button5.Location = new System.Drawing.Point(89, 402);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(48, 36);
+            this.button5.TabIndex = 6;
+            this.button5.Tag = "2";
+            this.button5.Text = "2";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Visible = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.button6.Location = new System.Drawing.Point(143, 402);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(48, 36);
+            this.button6.TabIndex = 7;
+            this.button6.Tag = "3";
+            this.button6.Text = "3";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Visible = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(543, 415);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 18);
+            this.label3.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(297, 107);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(168, 63);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Vége!";
+            this.label4.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label5.Location = new System.Drawing.Point(313, 212);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 25);
+            this.label5.TabIndex = 10;
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.button7.Location = new System.Drawing.Point(318, 330);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(138, 57);
+            this.button7.TabIndex = 11;
+            this.button7.Text = "új játék";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Visible = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Green;
+            this.ClientSize = new System.Drawing.Size(829, 450);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ 21/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _21
+{
+    public partial class Form1 : Form
+    {
+        public PictureBox[] tabla;
+        public int db = 21;
+        public int felhuzott = 0;
+        public int[] fontos_mezok = new int[5];
+        public int[] lepesek = new int[3];
+        public Random r = new Random();
+        public bool gepi = false;
+        public int korok = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            for (int i = 0; i < 5; i++)
+            {
+                fontos_mezok[i] = i * 4;
+                if (i < 3)
+                {
+                    lepesek[i] = i + 1;
+                }
+            }
+            kezdes();
+        }
+        private void kezdes()
+        {
+            Bitmap gyufa = new Bitmap("gyufa.png");
+           
+            tabla = new PictureBox[21];
+            for (int i = 0; i < 21; i++)
+            {
+
+                tabla[i] = new PictureBox();
+                tabla[i].SizeMode = PictureBoxSizeMode.StretchImage;
+                tabla[i].Image = (Image)gyufa;
+                tabla[i].Left = 50 + i * 35;
+                tabla[i].Top = 100;
+                this.Controls.Add(tabla[i]);
+                tabla[i].Width = 50;
+                tabla[i].Height = 150;
+            }
+            label2.Text = ($"{db} gyufa van hátra");
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (label1.Visible == false)
+            {
+                label1.Visible = true;
+
+            }
+            else
+            {
+                label1.Visible = false;
+            }
+        }
+
+        private void ember_lepes(int tag)
+        {
+            if (db>=tag)
+            {
+                for (int i = 0; i < tag; i++)
+                {
+
+                    tabla[felhuzott].Visible = false;
+                    felhuzott++;
+                    db--;
+                }
+                korok++;
+                vege();
+            }
+           
+        }
+
+        private void gep_lepes()
+        {
+            bool tipp = true;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (db - lepesek[j] == fontos_mezok[i])
+                    {
+                        for (int k = 0; k < lepesek[j]; k++)
+                        {
+                            tabla[felhuzott].Visible = false;
+                            felhuzott++;
+                            db--;
+                        }
+                        tipp = false;
+                        label3.Text = ($"A gép {lepesek[j]} db gyufát szedett fel");
+                    }
+                }
+            }
+            if (tipp == true)
+            {
+                int random = r.Next(1, 4);
+                for (int i = 0; i < random; i++)
+                {
+                    tabla[felhuzott].Visible = false;
+                    felhuzott++;
+                    db--;
+                }
+                label3.Text = ($"A gép {random} db gyufát szedett fel");
+            }
+            label2.Text = ($"{db} gyufa van hátra");
+            korok++;
+            vege();
+        }
+
+        public void vege()
+        {
+            if (db == 0)
+            {
+                button4.Visible = false;
+                button5.Visible = false;
+                button6.Visible = false;
+                label2.Visible = false;
+                label3.Visible = false;
+                label4.Visible = true;
+                button7.Visible=true;
+                if (gepi == true && korok % 2 == 0)
+                {
+                    label5.Text = "A gép nyert ;)";
+                }
+
+                if (gepi == false && korok % 2 == 0)
+                {
+                    label5.Text = "A második játékos nyert";
+                }
+                if (gepi == true && korok % 2 != 0)
+                {
+                    label5.Text = "Az ember nyert... :O";
+                }
+                if (gepi == false && korok % 2 != 0)
+                {
+                    label5.Text = "Az első játékos nyert";
+                }
+
+            }
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Visible = false;
+            button3.Visible = false;
+            label2.Visible = true;
+            button4.Visible = true;
+            button5.Visible = true;
+            button6.Visible = true;
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            gepi = true;
+            button2.Visible = false;
+            button3.Visible = false;
+            label2.Visible = true;
+            button4.Visible = true;
+            button5.Visible = true;
+            button6.Visible = true;
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int tag = Convert.ToInt32(button4.Tag);
+            ember_lepes(tag);
+            label2.Text = ($"{db} gyufa van hátra");
+            if (gepi == true && db != 0)
+            {
+                gep_lepes();
+            }
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int tag = Convert.ToInt32(button5.Tag);
+            ember_lepes(tag);
+            label2.Text = ($"{db} gyufa van hátra");
+            if (gepi == true && db != 0)
+            {
+                gep_lepes();
+            }
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int tag = Convert.ToInt32(button6.Tag);
+            ember_lepes(tag);
+            label2.Text = ($"{db} gyufa van hátra");
+            if (gepi == true && db != 0)
+            {
+                gep_lepes();
+            }
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+            this.Controls.Clear();
+            InitializeComponent();
+            db = 21;
+            felhuzott = 0;
+            gepi = false;
+            korok = 0;
+            kezdes();
+        }
+    }
+}
